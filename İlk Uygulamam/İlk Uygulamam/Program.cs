cf19822d3cb7189171504112cd2e4e398428c1df
gamze bilge <95417672+GamzeBilge1@users.noreply.github.com>
2024-12-17 21:43:44 +0300

    Delete İlk Uygulamam directory

--- İlk Uygulamam/İlk Uygulamam/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿
-Console.WriteLine("Gamze BİLGE ");
-Console.WriteLine("Patika Bootcamp'ine katılma nedenim, yazılım dünyasında kendimi geliştirmek, yeni teknolojiler öğrenmek ve profesyonel kariyerimde sağlam bir temel oluşturmak.");
